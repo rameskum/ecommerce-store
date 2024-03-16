12d973c0a73f07be9e5c7c0ee16403cb5791e21f
Ramesh Kumar <rameskum.ms@outlook.com>
2024-03-15 21:53:41 -0400

    billboard component and api to fetch billboard

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-	return <div>Home Store</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+	const billboard = await getBillboard('e6a62de1-331f-45d2-9384-98678c396f62');
+	return (
+		<Container>
+			<div className="space-yt-10 pb-10">
+				<Billboard data={billboard} />
+			</div>
+		</Container>
+	);
 };
 
 export default HomePage;
