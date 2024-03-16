4c985e92988e22aa8774d885f0f0b5896ff2ee1e
Ramesh Kumar <rameskum.ms@outlook.com>
2024-03-16 16:08:14 -0400

    product card and query string dependency added

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,15 +1,22 @@
 import getBillboard from '@/actions/get-billboard';
+import getProducts from '@/actions/get-products';
 import Billboard from '@/components/billboard';
+import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 
 export const revalidate = 0;
 
 const HomePage = async () => {
 	const billboard = await getBillboard('e6a62de1-331f-45d2-9384-98678c396f62');
+	const products = await getProducts({ isFeatured: true });
+
 	return (
 		<Container>
 			<div className="space-yt-10 pb-10">
 				<Billboard data={billboard} />
+				<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+					<ProductList title="Featured Products" items={products} />
+				</div>
 			</div>
 		</Container>
 	);
